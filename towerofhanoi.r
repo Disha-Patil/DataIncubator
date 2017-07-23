2718e6b66af5907ed5f8987ce0fbd7374d73db93
Disha-Patil <dishapatil10@yahoo.com>
2017-07-23 10:53:51 +0530

    Update towerofhanoi.r

--- towerofhanoi.r
+++ towerofhanoi.r
@@ -11,10 +11,12 @@ hanoi<-function(n,m)
 hanoi.move<-function(stacks,moves,n,m)
 {
   for(t in 1:moves){
+    print(t)
       #first elements only in each stack
         first<-sapply(stacks,"[[", 1)
         valid<-vector("list");loop=1
                   for(i in 1:m){
+                    print(i)
                             if(i==1){
                               if(is.null(first[[i+1]])){
                                   valid[[loop]]<-c(i,i+1); loop=loop+1}
