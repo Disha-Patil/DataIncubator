7471d616a6daa65d0b366f108af6850d669ff90b
Disha-Patil <dishapatil10@yahoo.com>
2017-07-23 19:36:51 +0530

    Update lendingloan.r

--- lendingloan.r
+++ lendingloan.r
@@ -1,1 +1,13 @@
+#read the 2014 and 2014 csv file containg issued loan data
+loan14<-read.csv("2014loan.csv")
+loan15<-read.csv("2015loan.csv")
+
+#join the two files for complete analysis
+loan.data<-rbind(loan14,loan15)
+
+#calculate median of loan amount issued after filtering out the NA values
+loan.median<-median(loan.data[-which(is.na(loan.data[,'loan_amnt'])),'loan_amnt'])
+
+#print the median loan amount
+cat("median loan amount = ",loan.median,"\n")
 
