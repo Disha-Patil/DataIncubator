24e1412c3128404eba9f975f56aec6cdf27d4c55
Disha-Patil <dishapatil10@yahoo.com>
2017-07-23 10:48:55 +0530

    Update towerofhanoi.r

--- towerofhanoi.r
+++ towerofhanoi.r
@@ -11,34 +11,37 @@ hanoi<-function(n,m)
 hanoi.move<-function(stacks,moves,n,m)
 {
   for(t in 1:moves){
-  #first elements only in each stack
-    first<-sapply(stacks,"[[", 1)
-    valid<-vector("list");loop=1
-      for(i in 1:m){
-        if(i==1){
-          if(is.null(first[[i+1]])){
-              valid[[loop]]<-c(i,i+1); loop=loop+1}
-          else if(first[[i]]<first[[i+1]]){
-              valid[[loop]]<-c(i,i+1); loop=loop+1}
-        }
+      #first elements only in each stack
+        first<-sapply(stacks,"[[", 1)
+        valid<-vector("list");loop=1
+                  for(i in 1:m){
+                            if(i==1){
+                              if(is.null(first[[i+1]])){
+                                  valid[[loop]]<-c(i,i+1); loop=loop+1}
+                              else if(first[[i]]<first[[i+1]]){
+                                  valid[[loop]]<-c(i,i+1); loop=loop+1}
+                            }
+                            else if(i==m){
+                                    if(is.null(first[[i-1]])){
+                                        valid[[loop]]<-c(i,i-1); loop=loop+1}
+                                    else if(first[[i]]<first[[i-1]]){
+                                        valid[[loop]]<-c(i,i-1); loop=loop+1}
+                            }
+                            else {      
+                                  if(is.null(first[[i+1]])){
+                                        valid[[loop]]<-c(i,i+1); loop=loop+1}
+                                  else if(first[[i]]<first[[i+1]]){
+                                        valid[[loop]]<-c(i,i+1); loop=loop+1}
+
+                                  if(is.null(first[[i-1]])){
+                                        valid[[loop]]<-c(i,i-1); loop=loop+1}
+                                  else if(first[[i]]<first[[i-1]]){
+                                  valid[[loop]]<-c(i,i-1); loop=loop+1}
+                                    
+                            }
+
+                }
         
-              
-        if(is.null(first[[i+1]])){
-              valid[[loop]]<-c(i,i+1); loop=loop+1}
-          else if(first[[i]]<first[[i+1]]){
-              valid[[loop]]<-c(i,i+1); loop=loop+1}
-        
-        if(is.null(first[[i-1]]) | first[[i]]<first[[i-1]]){
-        valid[[loop]]<-c(i,i-1); loop=loop+1}
-        
-        if(i==m){
-          if(is.null(first[[i-1]])){
-              valid[[loop]]<-c(i,i-1); loop=loop+1}
-          else if(first[[i]]<first[[i-1]]){
-              valid[[loop]]<-c(i,i-1); loop=loop+1}
-        }
-        
-      }
     
     
     
