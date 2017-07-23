ffebc0c212957662e3691acf592834f743e444a9
Disha-Patil <dishapatil10@yahoo.com>
2017-07-23 11:22:37 +0530

    Update towerofhanoi.r

--- towerofhanoi.r
+++ towerofhanoi.r
@@ -16,14 +16,18 @@ hanoi.move<-function(stacks,n,m)
         first<-sapply(stacks,"[[", 1)
         valid<-vector("list");loop=1
                   for(i in 1:m){
-                    #print(i);print(first)
+                    print(i);print(first)
                             if(i==1){
+                              if(is.null(first[[i]])){break}
+                              
                               if(is.null(first[[i+1]])){
                                   valid[[loop]]<-c(i,i+1); loop=loop+1}
                               else if(first[[i]]<first[[i+1]]){
                                   valid[[loop]]<-c(i,i+1); loop=loop+1}
                             }
                             else if(i==m){
+                                    if(is.null(first[[i]])){break}
+                              
                                     if(is.null(first[[i-1]])){
                                         valid[[loop]]<-c(i,i-1); loop=loop+1}
                                     else if(first[[i]]<first[[i-1]]){
@@ -51,13 +55,23 @@ hanoi.move<-function(stacks,n,m)
     return(valid)
   #}
 }
-  
+ 
   hanoi.all.moves<-function(stacks,moves,n,m)
   {
     for(t in 1:moves)
       {
         valid.moves<-hanoi.move(stacks,n,m)
         select.move<-sample(1:length(valid.moves),1)
-        print(select.move);print(valid.moves)
+        #print(select.move);print(valid.moves)
+        from<-valid.moves[[select.move]][1]
+        to<-valid.moves[[select.move]][2]
+        stacks[[to]]<-stacks[[from]][1]
+        stacks[[from]]<-stacks[[from]][-1]
+        print(stacks)
       } 
   }
+
+  
+  stacks<-hanoi(3,3)
+  hanoi.all.moves(stacks,6,3,3)
+  
