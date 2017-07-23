b68eca8f37d07eb22c7eb5f604d2c90759659cbd
Disha-Patil <dishapatil10@yahoo.com>
2017-07-23 11:00:18 +0530

    Update towerofhanoi.r

--- towerofhanoi.r
+++ towerofhanoi.r
@@ -8,15 +8,15 @@ hanoi<-function(n,m)
   return(stacks)
 }
 
-hanoi.move<-function(stacks,moves,n,m)
+hanoi.move<-function(stacks,moves=1,n,m)
 {
-  for(t in 1:moves){
-    print(t)
+  #for(t in 1:moves){
+    #print(t)
       #first elements only in each stack
         first<-sapply(stacks,"[[", 1)
         valid<-vector("list");loop=1
                   for(i in 1:m){
-                    print(i);print(first)
+                    #print(i);print(first)
                             if(i==1){
                               if(is.null(first[[i+1]])){
                                   valid[[loop]]<-c(i,i+1); loop=loop+1}
@@ -48,6 +48,6 @@ hanoi.move<-function(stacks,moves,n,m)
         
     
     
-    
-  }
+    return(valid)
+  #}
 }
